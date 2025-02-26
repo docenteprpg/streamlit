bcbe38120aef84849c65a2abe8c5901f006d143d
docenteprpg <docente.pr.pg@gmail.com>
2025-02-26 10:26:25 -0300

    Update atividadet1.py

--- atividadet1.py
+++ atividadet1.py
@@ -4,3 +4,10 @@ st.header('st.button')
 st.button('Say hello')
 st.toggle("Toggle")
 st.text_area("Enter text")
+st.slider('Quantos anos você tem?', 0, 130, 25)
+st.metric(
+    label="Métrica de Vendas",  # Nome ou título da métrica
+    value=100,          # Valor atual da métrica
+    delta=15,             # Variação em relação ao valor anterior
+    delta_color="inverse",      # Cor do delta (pode ser "normal", "inverse" ou "off")
+)
