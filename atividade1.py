59ef7dec8c23428860cb0841aefeb42665e9bae0
docenteprpg <docente.pr.pg@gmail.com>
2025-02-26 10:04:23 -0300

    Update atividade1.py

--- atividade1.py
+++ atividade1.py
@@ -1,6 +1,5 @@
-pip install matplotlib
+
 import streamlit as st
-import matplotlib.pyplot as plt
 import numpy as np
 
 # Título da aplicação
@@ -13,13 +12,6 @@ nome = st.text_input('Digite seu nome:', '')
 if nome:
     st.write(f'Olá, {nome}! Bem-vindo à nossa aplicação.')
 
-# Gerando dados para o gráfico
-x = np.arange(1, 6)
-y = np.random.randint(1, 10, size=5)
 
-# Exibindo um gráfico de barras
-fig, ax = plt.subplots()
-ax.bar(x, y)
 
-# Exibindo o gráfico na aplicação
-st.pyplot(fig)
+
